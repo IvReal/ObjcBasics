3b6860e7bc1701f982fa71d8caa281dce3fff960
IvReal <ivreal@yandex.ru>
2019-07-04 16:11:32 +0300

    Initial project

---
+++ ObjcBasics/ObjcBasics/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjcBasics
+//
+//  Created by Iv on 04/07/2019.
+//  Copyright © 2019 Iv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
