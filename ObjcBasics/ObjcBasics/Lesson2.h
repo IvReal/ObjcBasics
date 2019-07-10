e7c66a0bc40e954d2a291f08a7e859a7f676e9b0
IvReal <ivreal@yandex.ru>
2019-07-11 00:30:48 +0300

    Refactoring and preparations

---
+++ ObjcBasics/ObjcBasics/Lesson2.h
@@ -0,0 +1,12 @@
+//  Lesson2.h
+//  ObjcBasics
+//  Created by Iv on 11/07/2019.
+//  Copyright © 2019 Iv. All rights reserved.
+
+#ifndef Lesson2_h
+#define Lesson2_h
+
+void Lesson2Task1(void);
+void Lesson2Task2(void);
+
+#endif /* Lesson2_h */
---
+++ ObjcBasics/ObjcBasics/Lesson2.m
@@ -0,0 +1,18 @@
+//  Lesson2.m
+//  ObjcBasics
+//  Created by Iv on 11/07/2019.
+//  Copyright © 2019 Iv. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+// 1. Создание функции, которая будет проверять, входит ли переданная буква в английский алфавит.
+void Lesson2Task1(void)
+{
+    
+}
+
+//2. Разделение метода calculate (из практической задачи 2) на несколько методов (отдельно сложение, вычитание, умножение и деление).
+void Lesson2Task2(void)
+{
+    
+}
