8a39ce5d81a549c5ddf23449aba9a761b2552d26
IvReal <ivreal@yandex.ru>
2019-08-24 17:30:39 +0300

    Lesson8 tasks

---
+++ ObjcLiveCycle/ObjcLiveCycle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjcLiveCycle
+//
+//  Created by Iv on 24/08/2019.
+//  Copyright © 2019 Iv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ObjcLiveCycle/ObjcLiveCycle/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ObjcLiveCycle
+//
+//  Created by Iv on 24/08/2019.
+//  Copyright © 2019 Iv. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
